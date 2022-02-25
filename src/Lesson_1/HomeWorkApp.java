d3f394bb5819a0e615d31438eef304bd30b19cb1
IceLaim <100352879+IceLaim@users.noreply.github.com>
2022-02-25 17:11:04 +0300

    Домашнее задание 1

--- src/Lesson_1/HomeWorkApp.java
+++ src/Lesson_1/HomeWorkApp.java
@@ -5,6 +5,8 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
+        compareNumbers();
     }
 
     public static void printThreeWords() {
@@ -24,6 +26,27 @@ public class HomeWorkApp {
         }
 
     }
-
+    public static void printColor() {
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 & value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
     }
+
+        public static void compareNumbers() {
+            int a = 18;
+            int b = 15;
+            if (a >= b) {
+                System.out.println("a >= b");
+            } else {
+                System.out.println("a < b");
+            }
+        }
+    }
+
+
 
